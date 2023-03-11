57833d98b357c35abea83a2a6efd63a82f4c5399
Iván Villena González <ivan.villenag@gmail.com>
2023-03-11 09:46:28 +0100

    actualizacion ejercicios

--- exercise..arrays-2.js
+++ exercise..arrays-2.js
@@ -7,8 +7,8 @@ const puntajes = [
 
 // Crea una copia de la segunda fila del array y asignalo a una variable nueva.
 
-const segundaFila = [puntajes[1]];
-console.log(segundaFila);
+const copia = [puntajes[1]];
+
 
 // Elimina la segunda fila del array original.
 
@@ -20,11 +20,11 @@ puntajes[0].pop();
 
 // Agrega el numero 50 al final de la tercera fila del array original.
 
-puntajes[2].push(50);
+puntajes[1].push(50);
 
 // Eliminar el primer elemento de la tercera fila del array original.
 
-puntajes[2].shift();
+puntajes[1].shift();
 
 // Agrega el numero 100 al principio de la primera fila del array original.
 
@@ -33,4 +33,4 @@ puntajes[0].unshift(100);
 // Muestra por consola el array original y la copia de la segunda fila creada en el paso 2 para verificar los cambios.
 
 console.log(puntajes);
-console.log(segundaFila);
+console.log(copia);
